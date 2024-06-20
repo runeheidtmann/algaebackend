fe83ce9dbc6586e8bbe34b0b530af2a53144b9a1
Rune Heidtmann <heidtmann@gmail.com>
2024-06-20 08:41:53 +0200

    Security update

--- algaebackend/settings.py
+++ algaebackend/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@jt1%#&j^l(mlpfgnmf=yweh2+l-mb@d#88c@1955_5y)d)fde'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -100,9 +104,7 @@ WSGI_APPLICATION = 'algaebackend.wsgi.application'
 #     }
 # }
 
-from dotenv import load_dotenv
-# Load environment variables from .env file
-load_dotenv()
+
 
 DATABASES = {
     'default': {
