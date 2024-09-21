a760aac5b71071855cc89b9740d5246e7bf4357e
Rune Heidtmann <heidtmann@gmail.com>
2024-09-21 14:56:40 +0200

    userinfo updated

--- algaebackend/serializers.py
+++ algaebackend/serializers.py
@@ -4,7 +4,13 @@ from .models import Evaluation,LLM, Document,DocumentFile
 from django.contrib.auth.models import User
 
 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email','first_name','last_name']
 class EvaluationSerializer(serializers.ModelSerializer):
+    user = UserSerializer(readonly=True)
     class Meta:
         model = Evaluation
         fields = '__all__'
@@ -22,10 +28,6 @@ class LLMSerializer(serializers.ModelSerializer):
         model = LLM
         fields = ['name','description']
         
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email','first_name','last_name']
         
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
