876cee1b1206e5466c31bb3f4ade352395aeb95e
Rune Heidtmann <heidtmann@gmail.com>
2024-09-21 16:02:30 +0200

    user data

--- algaebackend/serializers.py
+++ algaebackend/serializers.py
@@ -1,34 +1,34 @@
 from rest_framework import serializers
 from rest_framework.serializers import Serializer, FileField
-from .models import Evaluation,LLM, Document,DocumentFile
+from .models import Evaluation, LLM, Document, DocumentFile
 from django.contrib.auth.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        extra_kwargs = {'password': {'write_only': True}}
 
 class EvaluationSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    
     class Meta:
         model = Evaluation
         fields = '__all__'
         extra_kwargs = {'user': {'read_only': True}}
 
     def create(self, validated_data):
-        # Get the user from the request
         user = self.context['request'].user
-        # Create a new Evaluation instance with the user
-        return Evaluation.objects.create(user=user, **validated_data)    
-    
-    
+        return Evaluation.objects.create(user=user, **validated_data)
+
 class LLMSerializer(serializers.ModelSerializer):
     class Meta:
         model = LLM
-        fields = ['name','description']
-        
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email','first_name','last_name']
-        
+        fields = ['name', 'description']
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password']
@@ -39,23 +39,24 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             validated_data['email'],
             validated_data['password']
         )
+        user.first_name = validated_data.get('first_name', '')
+        user.last_name = validated_data.get('last_name', '')
+        user.save()
         return user
 
 class DocumentFileSerializer(serializers.ModelSerializer):
     class Meta:
         model = DocumentFile
-        fields = ['id', 'file','document']
+        fields = ['id', 'file', 'document']
 
 class DocumentSerializer(serializers.ModelSerializer):
     document_files = DocumentFileSerializer(many=True, read_only=True, source='documentfile_set')
 
     class Meta:
         model = Document
-        fields = ['id', 'title','document_files'] 
+        fields = ['id', 'title', 'document_files']
 
     def create(self, validated_data):
-        # Get the user from the context (passed in the view)
         user = self.context.get('request').user
-        # Create a new Document instance with the user
         document = Document.objects.create(user_id=user.id, **validated_data)
         return document
--- algaebackend/views/EvaluationView.py
+++ algaebackend/views/EvaluationView.py
@@ -9,7 +9,7 @@ from rest_framework import status
 class EvaluationView(APIView):
     def get(self, request, format=None):
         evaluations = Evaluation.objects.all()
-        serializer = EvaluationSerializer(evaluations, many=True)
+        serializer = EvaluationSerializer(evaluations, many=True, context={'request': request})
         return Response(serializer.data)
 
     def post(self, request, format=None):
@@ -18,40 +18,14 @@ class EvaluationView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    
-class EvaluationDetailView(APIView):
 
-    # Return a model with primary key = id
+class EvaluationDetailView(APIView):
     def get(self, request, id):
         try:
             evaluation = Evaluation.objects.get(id=id)
         except Evaluation.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         
-        serializer = EvaluationSerializer(evaluation)
+        serializer = EvaluationSerializer(evaluation, context={'request': request})
         return Response(serializer.data)
-
-    # Update a particular model object with primary key = id
-    def put(self, request, id):
-        try:
-            evaluation = Evaluation.objects.get(pk=id)
-        except Evaluation.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = EvaluationSerializer(evaluation, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    # Delete a model object with id
-    def delete(self, request, id):
-        try:
-            evaluation = Evaluation.objects.get(pk=id)
-        except Evaluation.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        
-        evaluation.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
  
