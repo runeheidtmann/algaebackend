6e3e8aae10836230f3c0296b4267db4ac2df9371
Rune Heidtmann <heidtmann@gmail.com>
2025-11-09 21:03:52 +0100

    langchain version fix

--- algaebackend/views/RagChatView.py
+++ algaebackend/views/RagChatView.py
@@ -337,7 +337,7 @@ class RagChatAPIView(APIView):
             # Build response to be handled in the frontend
             data = {
                 "question": query,
-                "docs": [doc for doc in combined_docs],
+                "docs": context,
                 "docs_expanded": {
                     "vector_chunks": [self._doc_to_dict(doc) for doc in seed_docs],
                     "graph_chunks": [self._doc_to_dict(doc) for doc in expanded_docs],
